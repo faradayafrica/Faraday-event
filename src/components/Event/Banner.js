2b3f0bf44804119be485269985165bb7fe9fdb6f
Nworie Kingsley <obizzkingsley@gmail.com>
2021-12-16 00:32:23 +0100

    add env file

--- src/components/Event/Banner.js
+++ src/components/Event/Banner.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React from 'react';
+import overlay from '../../images/home-banner.svg';
 
 function Banner({ bannerImage }) {
   console.log(bannerImage);
   return (
-    <div className={`h-[60vh] bg-[#00000079] flex items-center -z-10 `}>
-      <img
-        src={bannerImage}
-        alt=""
-        className="bg-cover w-full h-full object-cover -z-10"
-      />
-    </div>
+    <>
+      <div
+        style={{ position: 'relative' }}
+        className={`h-[60vh] bg-[#00000079] flex items-center -z-10 `}
+      >
+        <img
+          src={bannerImage}
+          alt=''
+          className='bg-cover w-full h-full object-cover -z-10'
+        />
+        <img
+          style={{ position: 'absolute', opacity: 0.6 }}
+          src={overlay}
+          alt=''
+          className='bg-cover w-full h-full object-cover -z-10'
+        />
+      </div>
+    </>
   );
 }
 
