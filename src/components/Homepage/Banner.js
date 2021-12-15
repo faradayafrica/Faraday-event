da9e6857b99880473c3200a4ddcbbb477d55ced2
jomefavourite <jfjomefavourite@gmail.com>
2021-12-15 14:42:30 +0100

    feat: complete homepage banner

--- src/components/Homepage/Banner.js
+++ src/components/Homepage/Banner.js
@@ -1,11 +1,18 @@
 import React from "react";
+import BgBottom from "../../images/bg-bottom.svg";
 
 function Banner() {
   return (
-    <div className="h-[60vh] bg-[#00000065] text-white bg-homeBanner bg-no-repeat bg-cover grid place-content-center">
-      <h1 className="text-center text-2xl  md:text-5xl container max-w-[784px]">
+    <div className="h-[60vh] bg-[#00000065] text-white bg-homeBanner bg-no-repeat bg-cover grid place-content-center relative">
+      <h1 className="text-center text-2xl  md:text-5xl font-bold md:leading-snug container max-w-[784px]">
         Empowering students by building tools that improves learning experience.
       </h1>
+
+      <img
+        src={BgBottom}
+        alt=""
+        className="hidden container absolute -bottom-[60px] left-0 right-0 md:block"
+      />
     </div>
   );
 }
