b484fb743ecb6cd50466a0c2069a76256c69a186
Nworie Kingsley <obizzkingsley@gmail.com>
2021-12-18 17:29:03 +0100

    unimportant

--- src/components/Homepage/Banner.js
+++ src/components/Homepage/Banner.js
@@ -1,17 +1,17 @@
-import React from "react";
-import BgBottom from "../../images/bg-bottom.svg";
+import React from 'react';
+import BgBottom from '../../images/bg-bottom.svg';
 
 function Banner() {
   return (
-    <div className="h-[60vh] bg-[#00000065] text-white bg-homeBanner bg-no-repeat bg-cover grid place-content-center relative">
-      <h1 className="text-center text-2xl  md:text-5xl font-bold md:leading-snug container max-w-[784px]">
+    <div className='h-[60vh] bg-[#00000065] text-white bg-homeBanner bg-no-repeat bg-cover grid place-content-center relative'>
+      <h1 className='text-center text-2xl  md:text-5xl font-bold md:leading-snug container max-w-[784px]'>
         Empowering students by building tools that improves learning experience.
       </h1>
 
       <img
         src={BgBottom}
-        alt=""
-        className="hidden container absolute -bottom-[60px] left-0 right-0 md:block"
+        alt=''
+        className='hidden container absolute -bottom-[60px] left-0 right-0 md:block'
       />
     </div>
   );
