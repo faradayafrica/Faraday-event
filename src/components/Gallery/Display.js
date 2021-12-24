f8c8db960d5e9f5fc62d9cc91c5e74eebcabe53c
jomefavourite <jfjomefavourite@gmail.com>
2021-12-24 10:31:28 +0100

    added animation to menu

--- src/components/Gallery/Display.js
+++ src/components/Gallery/Display.js
@@ -2,24 +2,31 @@ import React from "react";
 
 function Display({ images }) {
   return (
-    <div className=" md:my-8  min-h-screen">
-      <div className="overflow-y-auto scroll-bar ">
-        <div className="grid gap-2 [grid-template-columns:_repeat(2,1fr)] [grid-template-rows:_unset] md:[grid-template-rows:_repeat(3,15rem)] md:[grid-template-columns:_repeat(5,20rem)] [grid-column:_1/5]">
-          {images.length === 0
-            ? "Loading"
-            : images.map((image, i) => (
-                <div key={i} className="bg-black">
-                  <img
-                    src={image?.url}
-                    alt=""
-                    className="h-full w-full object-cover"
-                  />
-                  {/* {console.log(image?.fields?.images)} */}
-                </div>
-              ))}
+    <>
+      <div className="container">
+        <h2 className="text-2xl border-b border-primary md:pb-4 md:max-w-md md:text-5xl">
+          Memorabilia
+        </h2>
+      </div>
+      <div className="my-4 md:my-8 min-h-screen">
+        <div className="overflow-y-auto scroll-bar ">
+          <div className="grid gap-2 [grid-template-columns:_repeat(2,1fr)] [grid-template-rows:_unset] md:[grid-template-rows:_repeat(3,15rem)] md:[grid-template-columns:_repeat(8,20rem)] [grid-column:_1/5]">
+            {images.length === 0
+              ? "Loading"
+              : images.map((image, i) => (
+                  <div key={i} className="bg-black">
+                    <img
+                      src={image?.url}
+                      alt=""
+                      className="h-full w-full object-cover"
+                      loading="lazy"
+                    />
+                  </div>
+                ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -25,13 +25,29 @@ export default function Navigation({ event, gallery }) {
         </Link>
       ) : (
         <>
-          <button onClick={() => setOpen(!open)} className="md:hidden">
-            <img
+          <button
+            onClick={() => setOpen(!open)}
+            className="md:hidden ml-auto space-y-1"
+          >
+            <span
+              className={`block w-6 h-[2px] bg-black transition-transform duration-200 ${
+                open && "[transform:_translateY(8px)_rotate(45deg)]"
+              }`}
+            ></span>
+            <span
+              className={`block w-6 h-[2px] bg-black ${open && "opacity-0"}`}
+            ></span>
+            <span
+              className={`block w-6 h-[2px] bg-black transition-transform duration-200 ${
+                open && "[transform:_translateY(-4px)_rotate(-45deg)]"
+              }`}
+            ></span>
+            {/* <img
               src={open ? Close : Menu}
               alt="menu"
               aria-hidden="true"
               className="ml-auto"
-            />
+            /> */}
           </button>
 
           <div
@@ -39,10 +55,10 @@ export default function Navigation({ event, gallery }) {
               open
                 ? "absolute bg-[#f8f9fa] block py-4 top-8 left-0 right-0 z-[10000] md:static md:top-[unset] md:py-0"
                 : "hidden md:block "
-            } col-span-2 mt-4 md:col-auto md:justify-self-end md:mt-0`}
+            } col-span-2 mt-4 transition-all duration-200 md:col-auto md:justify-self-end md:mt-0`}
           >
             <ul
-              className={`items-center space-y-2 ${
+              className={`items-center space-y-2  ${
                 open ? "w-[90%] mx-auto md:mx-0 md:w-full" : ""
               } md:space-y-0 md:space-x-3 md: md:flex`}
             >
