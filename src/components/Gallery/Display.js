be935f724a8770337b51eb03c3aff5bc7f3d2242
jomefavourite <jfjomefavourite@gmail.com>
2021-12-24 01:33:40 +0100

    navigation fixed

--- src/components/Gallery/Display.js
+++ src/components/Gallery/Display.js
@@ -4,7 +4,7 @@ function Display({ images }) {
   return (
     <div className=" md:my-8  min-h-screen">
       <div className="overflow-y-auto scroll-bar ">
-        <div className="grid gap-2 [grid-template-columns:_repeat(2,1fr)] [grid-template-rows:_unset] md:[grid-template-rows:_repeat(3,15rem)] md:[grid-template-columns:_repeat(5,20rem)] [grid-column:_1/5;]">
+        <div className="grid gap-2 [grid-template-columns:_repeat(2,1fr)] [grid-template-rows:_unset] md:[grid-template-rows:_repeat(3,15rem)] md:[grid-template-columns:_repeat(5,20rem)] [grid-column:_1/5]">
           {images.length === 0
             ? "Loading"
             : images.map((image, i) => (
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,43 +1,59 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../images/logo.svg';
-import Menu from '../images/menu.svg';
-import Close from '../images/close.svg';
-import Back from '../images/back-arrow.svg';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/logo.svg";
+import Menu from "../images/menu.svg";
+import Close from "../images/close.svg";
+import Back from "../images/back-arrow.svg";
 
 export default function Navigation({ event, gallery }) {
   const [open, setOpen] = useState(false);
+
   return (
     <nav
-      className={`container flex justify-between items-center py-5 relative ${
-        event && 'md:hidden'
+      className={`container grid grid-cols-2 justify-between items-center py-5 ${
+        event && "md:hidden"
       }`}
     >
-      <Link to='/'>
-        <img src={Logo} alt='faraday logo' />
+      <Link to="/">
+        <img src={Logo} alt="faraday logo" className="w-[170px]" />
       </Link>
 
-
       {event || gallery ? (
-        <Link to="/" className="flex gap-3 items-center">
+        <Link to="/" className="flex gap-3 items-center justify-self-end">
           <img src={Back} alt="back" aria-hidden="true" />
           {gallery && <span>Return</span>}
-
         </Link>
       ) : (
         <>
-          <button onClick={() => setOpen(true)} className='md:hidden'>
-            <img src={open ? Close : Menu} alt='menu' aria-hidden='true' />
+          <button onClick={() => setOpen(!open)} className="md:hidden">
+            <img
+              src={open ? Close : Menu}
+              alt="menu"
+              aria-hidden="true"
+              className="ml-auto"
+            />
           </button>
 
-          <ul className={`items-center space-x-4 hidden md:flex`}>
-            <li className='navlink font-medium '>
-              <a href='#past-events'>Past Events</a>
-            </li>
-            <li className='navlink font-medium'>
-              <Link to='/gallery'>Gallery</Link>
-            </li>
-          </ul>
+          <div
+            className={`${
+              open
+                ? "absolute bg-[#f8f9fa] block py-4 top-8 left-0 right-0 z-[10000] md:static md:top-[unset] md:py-0"
+                : "hidden md:block "
+            } col-span-2 mt-4 md:col-auto md:justify-self-end md:mt-0`}
+          >
+            <ul
+              className={`items-center space-y-2 ${
+                open ? "w-[90%] mx-auto md:mx-0 md:w-full" : ""
+              } md:space-y-0 md:space-x-3 md: md:flex`}
+            >
+              <li className="navlink font-medium ">
+                <a href="#past-events">Past Events</a>
+              </li>
+              <li className="navlink font-medium">
+                <Link to="/gallery">Gallery</Link>
+              </li>
+            </ul>
+          </div>
         </>
       )}
     </nav>
