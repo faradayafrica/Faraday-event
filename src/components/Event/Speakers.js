92723dbcf1df139dcd7ee4cb85b9f8c70e808a43
jomefavourite <jfjomefavourite@gmail.com>
2021-12-24 16:03:01 +0100

    fixed speaker styling

--- src/components/Event/Speakers.js
+++ src/components/Event/Speakers.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { useParams } from 'react-router-dom';
-import base from '../../util';
-import SkeletonLoader from '../SkeletonLoader';
+import React, { useState, useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import base from "../../util";
+import SkeletonLoader from "../SkeletonLoader";
 
 const data = [
   {
-    title: 'Event Title',
-    desc: 'Lorem ipsum dolor',
+    title: "Event Title",
+    desc: "Lorem ipsum dolor",
     image:
-      'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+      "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
   },
   {
-    title: 'Event Title',
-    desc: 'Lorem ipsum dolor',
+    title: "Event Title",
+    desc: "Lorem ipsum dolor",
     image:
-      'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+      "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
   },
   {
-    title: 'Event Title',
-    desc: 'Lorem ipsum dolor',
+    title: "Event Title",
+    desc: "Lorem ipsum dolor",
     image:
-      'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+      "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
   },
 ];
 
@@ -29,36 +29,36 @@ function Speakers({ eventSpeakers }) {
 
   useEffect(() => {
     eventSpeakers &&
-      eventSpeakers.forEach(speaker => {
-        base('event_speaker').find(speaker, function (err, record) {
+      eventSpeakers.forEach((speaker) => {
+        base("event_speaker").find(speaker, function (err, record) {
           if (err) {
             console.error(err);
             return;
           }
-          setSpeakers(prevState => [...prevState, record]);
+          setSpeakers((prevState) => [...prevState, record]);
         });
       });
   }, [eventSpeakers]);
 
   return (
-    <section className='space-y-4'>
-      <h2 className='event__title'>Speakers</h2>
+    <section className="space-y-4">
+      <h2 className="event__title">Speakers</h2>
 
-      <div className='flex flex-wrap gap-2'>
+      <div className="flex flex-wrap gap-2">
         {speakers.length === 0 ? (
           <SkeletonLoader speakers />
         ) : (
           speakers?.map((speaker, i) => (
-            <div key={i}>
+            <div key={i} className="max-w-[150px]">
               <img
                 src={speaker?.fields?.profile_image[0].url}
                 alt={speaker?.fields?.full_name}
-                className='w-[150px] h-[150px] object-cover mx-auto mb-2'
+                className="w-[150px] h-[150px] object-cover mx-auto mb-2"
               />
-              <h4 className='text-center font-medium'>
+              <h4 className="text-center font-medium">
                 {speaker?.fields?.full_name}
               </h4>
-              <p className='text-center font-medium text-[#3f3f41b3]'>
+              <p className="text-center font-medium text-[#3f3f41b3]">
                 {speaker?.fields?.title}
               </p>
             </div>
