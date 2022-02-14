1ec784c99f981abd4331f6ee0ee398e31d5ec85b
jomefavourite <jfjomefavourite@gmail.com>
2022-02-14 15:48:48 +0100

    phone number field added

--- src/components/Event/Form.js
+++ src/components/Event/Form.js
@@ -21,6 +21,7 @@ function Form({ eventId, eventCompleted, setToast }) {
       first_name: data.firstName,
       last_name: data.lastName,
       email: data.email,
+      phone_number: data.phone,
     };
 
     base("attendee").create(
@@ -44,64 +45,87 @@ function Form({ eventId, eventCompleted, setToast }) {
     );
   };
   return (
-    <div className="mt-12 md:-mt-12 relative" id="register">
-      <div className="bg-white sticky top-6 md:shadow p-5 md:max-w-sm mx-auto md:ml-auto form-container">
+    <div className='mt-12 md:-mt-12 relative' id='register'>
+      <div className='bg-white sticky top-6 md:shadow p-5 md:max-w-sm mx-auto md:ml-auto form-container'>
         {eventCompleted ? (
           <ClosedBanner />
         ) : (
           <div>
-            <h3 className="text-2xl font-semibold mt-2 mb-6 text-center">
+            <h3 className='text-2xl font-semibold mt-2 mb-6 text-center'>
               Register for this event
             </h3>
-            <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
+            <form className='space-y-5' onSubmit={handleSubmit(onSubmit)}>
               <input
-                type="text"
+                type='text'
                 {...register("firstName", { required: true })}
-                id="firstName"
-                placeholder="First name"
+                id='firstName'
+                placeholder='First name'
                 disabled={eventCompleted ? true : false}
                 className={`form-input ${
                   eventCompleted ? "cursor-not-allowed" : ""
                 } bg-[#c4c4c454]`}
               />
               {errors.firstName?.type === "required" && (
-                <span className="text-red-500 text-sm">
+                <span className='text-red-500 text-sm'>
                   First name is required
                 </span>
               )}
               <input
-                type="text"
+                type='text'
                 {...register("lastName", { required: true })}
-                id="lastName"
-                placeholder="Last name"
+                id='lastName'
+                placeholder='Last name'
                 disabled={eventCompleted ? true : false}
                 className={`form-input ${
                   eventCompleted ? " cursor-not-allowed" : ""
                 } bg-[#c4c4c454]`}
               />
               {errors.lastName?.type === "required" && (
-                <span className="text-red-500 text-sm">
+                <span className='text-red-500 text-sm'>
                   Last Name is required
                 </span>
               )}
               <input
-                type="email"
+                type='email'
                 {...register("email", {
                   required: true,
                   pattern: /^\S+@\S+$/i,
                 })}
-                id="email"
-                placeholder="Email"
+                id='email'
+                placeholder='Email'
                 disabled={eventCompleted ? true : false}
                 className={`form-input ${
                   eventCompleted ? " cursor-not-allowed" : ""
                 } bg-[#c4c4c454]`}
               />
               {errors.email?.type === "required" && (
-                <span className="text-red-500 text-sm">Email is required</span>
+                <span className='text-red-500 text-sm'>Email is required</span>
+              )}
+              <input
+                type='number'
+                {...register("phone", {
+                  required: true,
+                  pattern: /^\d{11,11}$/,
+                })}
+                id='phone'
+                placeholder='Phone Number'
+                disabled={eventCompleted ? true : false}
+                className={`form-input ${
+                  eventCompleted ? " cursor-not-allowed" : ""
+                } bg-[#c4c4c454]`}
+              />
+              {errors.phone?.type === "required" && (
+                <span className='text-red-500 text-sm'>
+                  Phone number is required
+                </span>
+              )}
+              {errors.phone?.type === "pattern" && (
+                <span className='text-red-500 text-sm'>
+                  Phone number entered isn't correct
+                </span>
               )}
               <button
-                type="submit"
+                type='submit'
                 disabled={eventCompleted ? true : false}
                 className={`flex justify-center items-center text-white  p-4 w-full ${
                   eventCompleted
@@ -111,23 +135,23 @@ function Form({ eventId, eventCompleted, setToast }) {
               >
                 {loading && (
                   <svg
-                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                    className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
                   >
                     <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
+                      className='opacity-25'
+                      cx='12'
+                      cy='12'
+                      r='10'
+                      stroke='currentColor'
+                      strokeWidth='4'
                     ></circle>
                     <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      className='opacity-75'
+                      fill='currentColor'
+                      d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
                     ></path>
                   </svg>
                 )}
