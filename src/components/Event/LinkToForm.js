e603c4c65b3230d3adaf7f32e499b6026c891d88
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2024-02-01 15:56:37 +0100

    adjust text size and button width

--- src/components/Event/LinkToForm.js
+++ src/components/Event/LinkToForm.js
@@ -1,10 +1,10 @@
 const LinkToForm = ({ link }) => {
   return (
     <div>
-      <h4 className="text-lg text-center font-semibold">
+      <h3 className="text-2xl font-semibold mt-2 mb-6 text-center">
         {/* Follow the link below to register for this event. */}
         Click the button below to register for this event.
-      </h4>
+      </h3>
 
       {/* <a
         href={link}
@@ -15,12 +15,12 @@ const LinkToForm = ({ link }) => {
         <h3 className="mt-4">{link}</h3>
       </a> */}
 
-      <h3 className="mt-4 object-fill  w-max rounded-[20px] text-white mx-auto">
+      <h3 className="space-y-5 flex justify-center items-center text-white  p-4 w-full">
         <a
           href={link}
           target="_blank"
           rel="noreferrer"
-          className="lnk px-3 py-2 bg-primary text-center rounded-[20px] font-semibold mt-4 w-auto"
+          className="lnk p-5 bg-primary text-center font-semibold w-full"
         >
           Register
         </a>
